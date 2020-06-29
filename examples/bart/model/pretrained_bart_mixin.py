7a431942f5658d20cad9ff803484b52061926d51
bwt09 <tanbowen@sjtu.edu.cn>
2020-06-29 05:44:22 -0400

    updated.

--- examples/bart/model/pretrained_bart_mixin.py
+++ examples/bart/model/pretrained_bart_mixin.py
@@ -1,3 +1,7 @@
+import os
+
+import torch
+
 from texar.torch.modules.pretrained.pretrained_base import PretrainedMixin
 
 
@@ -6,6 +10,9 @@ class PretrainedBARTMixin(PretrainedMixin):
     _MODEL2URL = {
         'bart.large': 'https://dl.fbaipublicfiles.com/fairseq/models/bart.large.tar.gz'
     }
+    _MODEL2CKPT = {
+        'bart.large': 'model.pt'
+    }
 
     def __init__(self):
         pass
@@ -13,10 +20,17 @@ class PretrainedBARTMixin(PretrainedMixin):
     def init_pretrained_weights(self, pretrained_model_name, cache_dir):
         self.pretrained_model_dir = self.download_checkpoint(
             pretrained_model_name, cache_dir)
+        self._init_from_checkpoint(pretrained_model_name=pretrained_model_name,
+                                   cache_dir=self.pretrained_model_dir)
 
     def _init_from_checkpoint(self, pretrained_model_name: str,
                               cache_dir: str, **kwargs):
-        pass
+        checkpoint_path = os.path.join(
+            cache_dir, self._MODEL2CKPT[pretrained_model_name])
+        ckpt_state_dict = torch.load(checkpoint_path)
+
+        for key, value in ckpt_state_dict:
+            print(key, value.shape)
 
     def _transform_config(cls, pretrained_model_name: str,
                           cache_dir: str):
