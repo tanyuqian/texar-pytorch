f26b6e6f9080f0e2db77f161a57dac4632bbbcfa
bwt09 <tanbowen@sjtu.edu.cn>
2020-06-29 02:05:00 -0400

    updated.

--- examples/bart/bart_test.py
+++ examples/bart/bart_test.py
@@ -2,16 +2,17 @@ import torch
 
 from model.bart_tokenizer import BARTTokenizer
 
-example = '''
-Texar-PyTorch is a toolkit aiming to support a broad set of machine learning, 
-especially natural language processing and text generation tasks. Texar provides
- a library of easy-to-use ML modules and functionalities for composing whatever 
- models and algorithms. The tool is designed for both researchers and 
- practitioners for fast prototyping and experimentation.'''
+example = '''Texar-PyTorch is a toolkit aiming to support a broad set of machine 
+learning, especially natural language processing and text generation tasks. 
+Texar provides a library of easy-to-use ML modules and functionalities for 
+composing whatever models and algorithms. The tool is designed for both 
+researchers and practitioners for fast prototyping and experimentation.'''
 
 bart = torch.hub.load('pytorch/fairseq', 'bart.large')
 
 tk = BARTTokenizer()
 
-print(tk.encode(example))
-print(bart.encode(example))
+if tk.encode(example) == bart.encode(example).tolist():
+    print('Tokenizer Check Passed.')
+else:
+    raise ValueError('Tokenizer Check Failed!!!')
