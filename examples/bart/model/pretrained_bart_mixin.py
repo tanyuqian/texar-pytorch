ff620e242bb350d502f7adeb480f2a02c1bb0772
bwt09 <tanbowen@sjtu.edu.cn>
2020-06-29 22:06:56 -0400

    updated.

--- examples/bart/model/pretrained_bart_mixin.py
+++ examples/bart/model/pretrained_bart_mixin.py
@@ -29,12 +29,56 @@ class PretrainedBARTMixin(PretrainedMixin):
             cache_dir, self._MODEL2CKPT[pretrained_model_name])
         ckpt_state_dict = torch.load(checkpoint_path)
 
-        self.state_dict()['token_embedder._embedding'].copy_(
-            ckpt_state_dict['encoder.embed_tokens.weight'])
-        self.state_dict()['encoder.pos_embedder.weight'].copy_(
-            ckpt_state_dict['encoder.embed_positions.weight'])
+        no_cond_map = {
+            'token_embedder._embedding': 'encoder.embed_tokens.weight',
+            'encoder.pos_embedder.weight': 'encoder.embed_positions.weight',
+            'encoder._transformer_encoder.input_normalizer.weight':
+                'encoder.layernorm_embedding.weight',
+            'encoder._transformer_encoder.input_normalizer.bias':
+                'encoder.layernorm_embedding.bias',
+        }
 
-    def _transform_config(cls, pretrained_model_name: str,
+        layer_c_map = {
+            'encoder._transformer_encoder.self_attns.{layer}.{c}_dense.weight':
+                'encoder.layers.{layer}.self_attn.{c}_proj.weight',
+            'encoder._transformer_encoder.self_attns.{layer}.{c}_dense.bias':
+                'encoder.layers.{layer}.self_attn.{c}_proj.bias'
+        }
+
+        layer_map = {
+            'encoder._transformer_encoder.output_layer_norm.{layer}.weight':
+                'encoder.layers.{layer}.final_layer_norm.weight',
+            'encoder._transformer_encoder.output_layer_norm.{layer}.bias':
+                'encoder.layers.{layer}.final_layer_norm.bias',
+            'encoder._transformer_encoder.poswise_networks.{layer}._layers.0.'
+            'weight': 'encoder.layers.{layer}.fc1.weight',
+            'encoder._transformer_encoder.poswise_networks.{layer}._layers.0.'
+            'bias': 'encoder.layers.{layer}.fc1.bias',
+            'encoder._transformer_encoder.poswise_networks.{layer}._layers.3.'
+            'weight': 'encoder.layers.{layer}.fc2.weight',
+            'encoder._transformer_encoder.poswise_networks.{layer}._layers.3.'
+            'bias': 'encoder.layers.{layer}.fc2.bias',
+            'encoder._transformer_encoder.poswise_layer_norm.{layer}.weight':
+                'encoder.layers.{layer}.self_attn_layer_norm.weight',
+            'encoder._transformer_encoder.poswise_layer_norm.{layer}.bias':
+                'encoder.layers.{layer}.self_attn_layer_norm.bias'
+        }
+
+        for temp_ours, temp_ckpt in no_cond_map.items():
+            self.state_dict()[temp_ours].copy_(ckpt_state_dict[temp_ckpt])
+
+        for layer in range(12):
+            for temp_ours, temp_ckpt in layer_map.items():
+                self.state_dict()[temp_ours.format(layer=layer)].copy_(
+                    temp_ckpt.format(layer=layer))
+
+            for c in ['Q', 'K', 'V', 'O']:
+                for temp_ours, temp_ckpt in layer_c_map.items():
+                    self.state_dict()[temp_ours.format(layer=layer, c=c)].copy_(
+                        temp_ckpt.format(layer=layer, c=c))
+
+
+def _transform_config(cls, pretrained_model_name: str,
                           cache_dir: str):
         pass
 
