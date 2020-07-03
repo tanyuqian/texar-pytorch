172f5a17ac473fe6aaed792049bee4a434d44e11
bwt09 <tanbowen@sjtu.edu.cn>
2020-07-02 22:42:18 -0400

    updated.

--- examples/bart/bart_test.py
+++ examples/bart/bart_test.py
@@ -13,7 +13,7 @@ Texar provides a library of easy-to-use ML modules and functionalities for
 composing whatever models and algorithms. The tool is designed for both
 researchers and practitioners for fast prototyping and experimentation.'''
 
-bart = BART(pretrained_model_name='bart.large.mnli')
+bart = BART(pretrained_model_name='bart.large.cnn')
 bart.eval()
 # input_ids = bart.encode(
 #     'BART is a sequence model.', 'BART is not sequence to sequence.')
@@ -39,10 +39,10 @@ lengths = torch.tensor([len(input_ids)])
 # print(bart.extract_features(tokens=tokens, lengths=lengths))
 # print(fs_bart.extract_features(tokens=tokens))
 
-# sample_id = bart.generate(
-#     src_tokens=tokens, src_lengths=lengths)['sample_id'][:, :, 0].view(-1).tolist()
-# print(sample_id)
-# print(bart.decode(sample_id))
+sample_id = bart.generate(
+    src_tokens=tokens, src_lengths=lengths)['sample_id'][:, :, 0].view(-1).tolist()
+print(sample_id)
+print(bart.decode(sample_id))
 
 print(fs_bart.sample([example], beam=4, lenpen=2.0, max_len_b=140))
 
